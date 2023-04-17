74f6c52c7e66af4833cd164b8101384ceaac8365
faypiya <122408123+faypiya@users.noreply.github.com>
2023-04-17 00:15:29 -0400

    Add files via upload

--- js/project1.js
+++ js/project1.js
@@ -23,7 +23,12 @@ function openModal() {
   function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    
   }
   
